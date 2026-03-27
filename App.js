f31fa5c781961b61df4e98521db828f787552d5c
Jorge Luiz Madeira Pires <jorge.60424@alunosatc.edu.br>
2026-03-26 21:36:05 -0300

    att 1

--- App.js
+++ App.js
@@ -1,58 +1,14 @@
-import 'react-native-gesture-handler';
-import { useState } from 'react';
-import { PaperProvider } from 'react-native-paper';
+import 'react-native-gesture-handler'; 
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Calculadora from './components/calculadora.jsx';
-import Historico from './components/Historico.jsx';
-
-const Drawer = createDrawerNavigator();
+import { Provider as PaperProvider } from 'react-native-paper';
+import DrawerNav from './DrawerNav'; 
 
 export default function App() {
-  const [display, setDisplay] = useState('');
-  const [historico, setHistorico] = useState([]);
-
-  function onPressNumero(num) {
-    setDisplay(display + num);
-  }
-
-  function onPressOperador(op) {
-    setDisplay(display + ' ' + op + ' ');
-  }
-
-  function onPressIgual() {
-    try {
-      const resultado = String(eval(display));
-      setHistorico([...historico, display + ' = ' + resultado]);
-      setDisplay(resultado);
-    } catch {
-      setDisplay('Erro');
-    }
-  }
-
-  function onPressLimpar() {
-    setDisplay('');
-  }
-
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Calculadora">
-          <Drawer.Screen name="Calculadora">
-            {() => (
-              <Calculadora
-                display={display}
-                onPressNumero={onPressNumero}
-                onPressOperador={onPressOperador}
-                onPressIgual={onPressIgual}
-                onPressLimpar={onPressLimpar}
-              />
-            )}
-          </Drawer.Screen>
-          <Drawer.Screen name="Histórico">
-            {() => <Historico historico={historico} />}
-          </Drawer.Screen>
-        </Drawer.Navigator>
+        <DrawerNav />
       </NavigationContainer>
     </PaperProvider>
   );
